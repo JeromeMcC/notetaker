e0b064f1a29d6d8403475079fd4411a6e620d71f
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2022-02-17 12:39:04 -0500

    working on post request

--- Develop/server.js
+++ Develop/server.js
@@ -2,20 +2,57 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+const noteData = require('./db/db.json')
+const fs = require('fs')
+const  { v4: uuidv4 }= require ('uuid')
 //GET /notes should return the notes.html file.
 
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.get(express.json());
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static('public'))
+
+app.get('/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req,res) => {
-    res.json
+  res.json(noteData)
+});
+
+app.post('/api/notes', (req,res) => {
+    req.body
+    console.log (req.body)
+    let note ={
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4() 
+        
+    }
+    console.log(note)
+    fs.readFile('./db/db.json','utf8',(err, data) => {
+        if (err) throw err
+        let savedNotes = JSON.parse(data)
+        savedNotes.push(note)
+        fs.writeFile('./db/db.json',JSON.stringify(note), err =>{
+            if (err) throw err
+            
+            res.send(savedNotes);
+            console.log (savedNotes);
+        })
+    })
+    
+
 })
 
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 
-app.listen(3001, () => console.log('Express Server on port 3001!'));
+
+
+
+
+app.listen(PORT, () => console.log('Express Server on port 3001!'));
